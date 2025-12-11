a56f675c8582da28b90bd4dead77a814aa02a746
Sharon Xu <sharxist@gmail.com>
2025-12-11 07:29:15 -0500

    Fix manufacturing stats color alternation
    - Restore alternating orange/gray pattern
    - Orange: 0.02mm, 4-AXIS, 600ft²
    - Gray: ½", 3D

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -53,12 +53,12 @@ function About({ scrollY }) {
               <div className="text-5xl font-black font-mono mb-2 text-machine-orange">0.02mm</div>
               <div className="text-steel">Laser cutting precision</div>
             </div>
-            <div className="border-l-4 border-machine-orange pl-6">
-              <div className="text-5xl font-black font-mono mb-2 text-machine-orange">½"</div>
+            <div className="border-l-4 border-steel pl-6">
+              <div className="text-5xl font-black font-mono mb-2">½"</div>
               <div className="text-steel">Stainless steel cutting depth</div>
             </div>
-            <div className="border-l-4 border-steel pl-6">
-              <div className="text-5xl font-black font-mono mb-2">4-AXIS</div>
+            <div className="border-l-4 border-machine-orange pl-6">
+              <div className="text-5xl font-black font-mono mb-2 text-machine-orange">4-AXIS</div>
               <div className="text-steel">CNC mill & lathe capability</div>
             </div>
             <div className="border-l-4 border-steel pl-6">
